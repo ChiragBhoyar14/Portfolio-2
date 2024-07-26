3487fc92908a7b390eefb4a2e320400feb5186ac
ChiragBhoyar14 <149136136+ChiragBhoyar14@users.noreply.github.com>
2024-07-27 00:09:59 +0530

    config chang

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.{js,jsx,ts,tsx}', // Define the paths to all of the template files in your project
+    './src/**/*.jsx', // Define the paths to all of the template files in your project
   ],
   darkMode: 'class', // Enable class-based dark mode
   theme: {
